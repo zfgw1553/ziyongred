b296dd9c161e74ae9b94acd391999fa146d5968c
zfgw1553 <136322400+zfgw1553@users.noreply.github.com>
2023-07-27 22:13:08 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '966cb04c-e8ff-4cda-af74-65ccf97498d4';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
